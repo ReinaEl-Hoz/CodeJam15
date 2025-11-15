0c342e616447c6188fff99e94a4e3db697e6d907
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-11-15 13:03:26 -0500

    adding key insights code

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,11 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
 import duckdb
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 from key_insights import get_key_insights
 
 app = FastAPI()
 con = duckdb.connect("codejam_15.db")
 
-@app.get("/key-insights", response_class=HTMLResponse)
+@app.get("/key-insights", response_class=JSONResponse)
 def profile_report(query: str):
-    html_str = get_key_insights(query)
-    return HTMLResponse(content=html_str)
+    return get_key_insights(query)
