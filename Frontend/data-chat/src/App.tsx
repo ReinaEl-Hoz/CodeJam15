5f075d481b3ba9f09c7a22b7feb8d5ebefb454b8
Marianne Romero <marianne.romero@mail.mcgill.ca>
2025-11-15 14:04:37 -0500

    adding dashboard builder

--- Frontend/data-chat/src/App.tsx
+++ Frontend/data-chat/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import KeyInsights from "./KeyInsights";
 import "./App.css";
+import DashboardBuilder from "./DashboardBuilder";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/report/:reportId" element={<KeyInsights />} />
+        <Route path="/dashboard-builder" element={<DashboardBuilder/>}/>
       </Routes>
     </BrowserRouter>
   );
